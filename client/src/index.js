d69b45435edab9d33bc0ae673fca145a20e0be69
hafsaabdi880 <hafsaabdi88@outlook.com>
2023-07-04 17:58:19 +0100

    navbar

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,14 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
